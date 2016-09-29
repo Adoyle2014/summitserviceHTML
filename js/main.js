0ddeae8e7fa2d324e64e71b586a74c3322f2ac5b
Adam Doyle <dominofoe@gmail.com>
2016-09-29 19:29:00 -0400

    Customer Equipment added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
     // SideNav Initialization
     $(".button-collapse").sideNav();
+    // Select Initialization
+    $('.mdb-select').material_select();
+
+    tableRowRedirect();
+
+
 
 });
 
-
+function tableRowRedirect() {
+    $(".clickable-row").click(function () {
+        window.location = $(this).data("href");
+    });
+}
